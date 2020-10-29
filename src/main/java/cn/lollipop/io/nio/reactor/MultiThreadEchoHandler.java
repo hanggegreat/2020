389d1f1287d278d21e7f9c5353dee0a5336b00bd
lollipop <hanggegreat@vip.qq.com>
2020-10-29 20:39:05 +0800

    update

--- src/main/java/cn/lollipop/io/nio/reactor/MultiThreadEchoHandler.java
+++ src/main/java/cn/lollipop/io/nio/reactor/MultiThreadEchoHandler.java
@@ -18,10 +18,8 @@ import java.util.concurrent.TimeUnit;
  */
 @Slf4j
 public class MultiThreadEchoHandler {
-    private final ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 1, 0, TimeUnit.SECONDS, new ArrayBlockingQueue<>(0), new ThreadPoolExecutor.AbortPolicy());
-
-    private SocketChannel socketChannel;
-    private SelectionKey selectionKey;
+    private final SocketChannel socketChannel;
+    private final SelectionKey selectionKey;
 
     private final ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
 
@@ -30,37 +28,37 @@ public class MultiThreadEchoHandler {
 
     private int state = RECEIVING;
 
-    public MultiThreadEchoHandler(SocketChannel socketChannel, Selector selector) {
-        try {
-            this.socketChannel = socketChannel;
-            socketChannel.configureBlocking(false);
-            this.selectionKey = socketChannel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE, this);
-            selector.wakeup();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private final ThreadPoolExecutor executor = new ThreadPoolExecutor(10, 20, 0, TimeUnit.SECONDS, new ArrayBlockingQueue<>(100), new ThreadPoolExecutor.AbortPolicy());
+
+    public MultiThreadEchoHandler(SocketChannel socketChannel, Selector selector) throws IOException {
+        this.socketChannel = socketChannel;
+        socketChannel.configureBlocking(false);
+        this.selectionKey = socketChannel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE, this);
+        selector.wakeup();
     }
 
     public void start() {
         executor.submit(() -> {
-            try {
-                if (state == SENDING) {
-                    socketChannel.write(byteBuffer);
-                    byteBuffer.clear();
-                    selectionKey.interestOps(SelectionKey.OP_READ);
-                    state = RECEIVING;
-                } else {
-                    int length;
-                    while ((length = socketChannel.read(byteBuffer)) > 0) {
-                        log.info(new String(byteBuffer.array(), 0, length));
-                    }
+            synchronized (this) {
+                try {
+                    if (state == SENDING) {
+                        socketChannel.write(byteBuffer);
+                        byteBuffer.clear();
+                        selectionKey.interestOps(SelectionKey.OP_READ);
+                        state = RECEIVING;
+                    } else {
+                        int length;
+                        while ((length = socketChannel.read(byteBuffer)) > 0) {
+                            log.info(new String(byteBuffer.array(), 0, length));
+                        }
 
-                    byteBuffer.flip();
-                    selectionKey.interestOps(SelectionKey.OP_WRITE);
-                    state = SENDING;
+                        byteBuffer.flip();
+                        selectionKey.interestOps(SelectionKey.OP_WRITE);
+                        state = SENDING;
+                    }
+                } catch (IOException e) {
+                    log.error(e.getMessage());
                 }
-            } catch (IOException e) {
-                log.error(e.getMessage());
             }
         });
     }
--- src/main/java/cn/lollipop/io/nio/reactor/MultiThreadEchoServerReactor.java
+++ src/main/java/cn/lollipop/io/nio/reactor/MultiThreadEchoServerReactor.java
@@ -11,21 +11,24 @@ import java.util.Iterator;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * reactor
+ *
  * @author zhangyuanhang
  */
 @Slf4j
 public class MultiThreadEchoServerReactor {
-
-    private final Selector selector;
     private final ServerSocketChannel serverSocketChannel;
+    private AtomicInteger next = new AtomicInteger();
+    private Selector[] selectors = new Selector[2];
     private final AcceptorHandler acceptorHandler;
-    private final ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 1, 0, TimeUnit.SECONDS, new ArrayBlockingQueue<>(0), new ThreadPoolExecutor.AbortPolicy());
+    private final ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 1, 0, TimeUnit.SECONDS, new ArrayBlockingQueue<>(10), new ThreadPoolExecutor.AbortPolicy());
 
     public MultiThreadEchoServerReactor(int port) throws IOException {
-        selector = Selector.open();
+        selectors[0] = Selector.open();
+        selectors[1] = Selector.open();
         serverSocketChannel = ServerSocketChannel.open();
         acceptorHandler = new AcceptorHandler();
         // 绑定端口
@@ -33,42 +36,46 @@ public class MultiThreadEchoServerReactor {
         // 设置非阻塞
         serverSocketChannel.configureBlocking(false);
         // 将新连接处理器作为附件，绑定到sk选择键
-        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT, acceptorHandler);
+        serverSocketChannel.register(selectors[0], SelectionKey.OP_ACCEPT, acceptorHandler);
     }
 
-    public void start() throws IOException {
-        executor.submit(() -> {
-            try {
-                while (!Thread.interrupted()) {
-                    int count = selector.select();
-                    if (count == 0) {
-                        continue;
-                    }
-
-                    Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
-                    // 遍历所有事件
-                    while (iterator.hasNext()) {
-                        SelectionKey key = iterator.next();
-
-                        // 有新的连接到达
-                        if (key.isAcceptable()) {
-                            dispatch(key);
+    public void start() {
+        for (Selector selector : selectors) {
+            executor.submit(() -> {
+                try {
+                    while (!Thread.interrupted()) {
+                        int count = selector.select();
+                        if (count == 0) {
+                            continue;
                         }
 
-                        // 移除key，防止重复处理
-                        iterator.remove();
+                        Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                        // 遍历所有事件
+                        while (iterator.hasNext()) {
+                            SelectionKey key = iterator.next();
+
+                            // 有新的连接到达
+                            if (key.isAcceptable()) {
+                                dispatch(key);
+                            }
+
+                            // 移除key，防止重复处理
+                            iterator.remove();
+                        }
                     }
+                } catch (IOException e) {
+                    log.error(e.getMessage());
                 }
-            } catch (IOException e) {
-                log.error(e.getMessage());
-            }
-        });
+            });
+        }
     }
 
-    private void dispatch(SelectionKey sk) throws IOException {
+    private void dispatch(SelectionKey sk) {
         AcceptorHandler handler = (AcceptorHandler) sk.attachment();
-        handler.accept(serverSocketChannel, selector);
+        handler.accept(serverSocketChannel, selectors[next.getAndSet(next.get() + 1 % selectors.length)]);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new MultiThreadEchoServerReactor(6869).start();
     }
 }
-
-
